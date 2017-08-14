fb9c0ef169cd449e2e5ee34370f60c2061a8692b
TexasBullet26 <lanzertrey@gmail.com>
2017-08-14 18:03:56 -0500

    Add Solution to freeCodeCamps Reverse a String
    freeCodeCamps Basic Algorithm Scripting solution for Reverse a String.

---
+++ Basic_Algorithm_Scripting-Solutions/reverseString.js
@@ -0,0 +1,25 @@
+/*
+ *  freeCodeCamp: Reverse a String
+ *
+ *  Reverse the provided string.
+ *
+ *  You may need to turn the string into an array before you can reverse it.
+ *
+ *  Your result must be a string
+ *
+ *  Helpful links:
+ *    - Global String Object
+ *    - Sting.prototype.split()
+ *    - Array.prototype.reverse()
+ *    - Array.prototype.join()
+ */
+
+function reverseString(str) {
+  // split str, reverse str, join reversed str.
+  return str.split('').reverse().join('');
+}
+
+// Tests
+reverseString( "hello" ); // should return a string "olleh".
+reverseString( "Howdy" ); // should return "ydwoH".
+reverseString( "Greetings from Earth" ); // should return "htraE morf sgniteerG".
